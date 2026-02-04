f41a0d4a73ec404777b4d3cea5e9502229015b29
Ritik Sharma <ritiksharma10508@gmail.com>
2026-02-05 03:59:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let timer = documenet.getElementById("displayTimer")
+
+let currentDate = new Date();
+
+setInterval(
+function(){
+	currentDate = new Date();
+	timer.innerHTML = currentDate.toLocaleString();
+		},1000
+)
+
